ec2ed06cd629eb3c50e1ea05fb4ac40a6cf5a321
Diablo9946 <diabloch9946@gmail.com>
2019-07-01 14:41:33 +0200

    Comments made.

---
+++ action.js
@@ -0,0 +1,19 @@
+var elemento = document.getElementById("mio_id");
+
+// Chiedo all'utente il numero di chilometri da percorrere
+
+
+// Chiedo l'eta del cliente
+
+
+// Stabilisco il prezzo del Biglietto
+
+
+// Stabilisco lo sconto per minorenni
+
+
+// Stablilisco lo sconto per gli anziani
+
+
+
+// Calcolo il prezzo del blilietto al cliente
