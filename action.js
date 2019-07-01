a0d32c5b2fed107e3662bd52a88c3266ea5bf1e9
Diablo9946 <diabloch9946@gmail.com>
2019-07-01 15:16:38 +0200

    Almost ready

--- action.js
+++ action.js
@@ -1,19 +1,25 @@
 var elemento = document.getElementById("mio_id");
 
 // Chiedo all'utente il numero di chilometri da percorrere
-
+var distanzadaper = prompt("Indica i chilometri da percorrere");
+console.log(distanzadaper);
 
 // Chiedo l'eta del cliente
-
+var etacliente = prompt("Quanti anni hai?");
+console.log(etacliente);
 
 // Stabilisco il prezzo del Biglietto
+var prezzobliglietto = distanzadaper * etacliente;
+console.log(prezzobliglietto);
 
 
 // Stabilisco lo sconto per minorenni
+var scontomin = etacliente <= 18;
+console.log(scontomin);
 
 
 // Stablilisco lo sconto per gli anziani
-
-
+var scontoanz = etacliente >= 65;
+console.log(scontoanz);
 
 // Calcolo il prezzo del blilietto al cliente
